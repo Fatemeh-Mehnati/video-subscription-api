cf33653b41d25c90ca33afa324027077bf73853c
Fatemeh Mehnati <fatemeh.mehnati20@gmail.com>
2026-09-22 20:23:55 +0330

    feat(videos): add watch endpoint with subscription-based access control

--- videos/views.py
+++ videos/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.db.models import F
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .filters import VideoFilter
 from .models import Category, Video
-from .permissions import IsAdminOrReadOnly
+from .permissions import CanWatchVideo, IsAdminOrReadOnly
 from .serializers import CategorySerializer, VideoSerializer
-
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -29,3 +32,15 @@ class VideoViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(uploaded_by=self.request.user)
+
+    @action(
+        detail=True,
+        methods=["get"],
+        permission_classes=(IsAuthenticated, CanWatchVideo),
+    )
+    def watch(self, request, pk=None):
+        video = self.get_object()
+        Video.objects.filter(pk=video.pk).update(views_count=F("views_count") + 1)
+        return Response(
+            {"id": video.id, "title": video.title, "video_url": video.video_url}
+        )
